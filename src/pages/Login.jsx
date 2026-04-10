220336f3d1e616adfa48d27d1d3da551c12afde7
essie266 <essieshan10@gmail.com>
2026-04-10 18:28:44 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import logo from "../assets/images/header/Logos.png";
+import logo from "../assets/images/header/logos.png";
 import csIcon from "../assets/images/header/cs1.png";
 import CustomerServiceModal from "../components/CustomerServiceModal";
 import "./Login.css";
